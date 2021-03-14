d61102cc6cd76e4686966020ff59e16f57a34b15
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-13 19:45:16 -0500

    added routes to app.js

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,17 +3,22 @@ import { json } from "body-parser";
 
 import {
   currentUserRouter,
-  signupRouter,
   signinRouter,
   signoutRouter,
+  signupRouter,
 } from "./routes";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8001;
 
 const app = express();
 
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}! \n url: http//localhost:8001`)
+);
