690e7dee70fa3496d41b2b88ce1b33f7e9e86fde
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-13 19:02:12 -0500

    added app.js file with default configuration

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,1 +1,19 @@
 import express from "express";
+import { json } from "body-parser";
+
+import {
+  currentUserRouter,
+  signupRouter,
+  signinRouter,
+  signoutRouter,
+} from "./routes";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(json());
+
+app.use(currentUserRouter);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
