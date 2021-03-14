1e935d83eeedc0f4dc487b762fcbf902a7d22318
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-13 19:01:17 -0500

    added basic authentication router

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/api/users/currentuser", () => {});
+
+export default router;
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/api/users/signup", () => {});
+
+export default router;
